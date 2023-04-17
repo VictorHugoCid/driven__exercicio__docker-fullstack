5344f90174c54989d5fc7e6d8459b173a62492d3
VictorCid <victorcidpereira@gmail.com>
2023-04-17 15:56:57 -0300

    fear: fallBack

--- front-end/src/components/App.jsx
+++ front-end/src/components/App.jsx
@@ -1,6 +1,7 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Home from "./Home";
 import Names from "./Names";
+import NotFoundComponent from './NotFoundComponent';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/"  element={<Home />} />
         <Route path="/names" element={<Names />} />
+        <Route path="/**" element={<NotFoundComponent />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ front-end/src/components/NotFoundComponent.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function NotFoundComponent() {
+  return (
+    <h1>
+      Not Found
+    </h1>
+  );
+}
+
